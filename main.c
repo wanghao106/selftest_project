f8c1778993954a68cc2b716e0258ae48d34d1f6f
wanghao106 <wang_hao106@163.com>
2024-01-06 16:21:01 +0800

    remove falut main.c

--- main.c
+++ main.c
@@ -7,19 +7,3 @@ main()
       printf("git test \n");
     }
 }
-main()
-{
-  int i = 0;
-  while(1)
-    {
-      printf("git test \n");
-    }
-}
-void test()
-{
-	sleep(1);
-
-}
-
-
-
--- three.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-void thr()
-{
-	sleep(1);
-}
